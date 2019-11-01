749101f243a7ce72ceb2a68152a209495fe36f48
czewail <chanzewail@gmail.com>
2019-11-01 09:57:11 +0800

    database: add mysql reconnector

--- packages/framework/src/database/connector/mysql-connector.ts
+++ packages/framework/src/database/connector/mysql-connector.ts
@@ -1,11 +1,28 @@
-import { createConnection, ConnectionConfig } from 'mysql'
+import { createConnection, ConnectionConfig, MysqlError, Connection } from 'mysql'
 import { ConnectorInterface } from './connector.interface'
 
 // Mysql 连接器
 export class MysqlConnector implements ConnectorInterface {
+
+  connection: Connection;
+
   connect(options: string | ConnectionConfig) {
     const connection = createConnection(options);
     connection.connect();
+    connection.on('error', (err: MysqlError) => {
+      this.handleConnectionLost(err, options)
+    });
     return connection
+  }
+
+
+  handleConnectionLost(err: MysqlError, options: string | ConnectionConfig) {
+    if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        this.connect(options)
+      } else {
+        throw err;
+      }
+    }
   }
 }
