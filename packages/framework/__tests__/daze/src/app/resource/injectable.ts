590c2ff718c98f9c3ee4516a011c83e5fbd39f3d
chenzhe.czw <chanzewail@gmail.com>
2020-05-08 14:30:37 +0800

    chore(style): update code style

--- packages/framework/__tests__/daze/src/app/resource/injectable.ts
+++ packages/framework/__tests__/daze/src/app/resource/injectable.ts
@@ -1,12 +1,8 @@
 import { BaseResource, http, component } from '../../../../../src';
 
-
 @component('injectable')
 export default class extends BaseResource {
-  resolve(
-    @http.query('id') id: number,
-      data: any
-  ) {
+  resolve(@http.query('id') id: number, data: any) {
     return {
       ...data,
       id,
