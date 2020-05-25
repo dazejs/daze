cd5b751694b81234ece0288dcfc94beee3859ca8
czewail <chanzewail@gmail.com>
2020-05-25 14:30:43 +0800

    chore(test): ignore messenger and cluster cover

--- packages/framework/jest.config.js
+++ packages/framework/jest.config.js
@@ -23,9 +23,11 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/src/cluster/",
+    "<rootDir>/src/messenger/",
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
